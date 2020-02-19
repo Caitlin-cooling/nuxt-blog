dce8479b42517907b797e06d0cf0b295d2ada248
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-02-19 11:53:04 +0000

    Updates home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <h1>Home</h1>
+    <h1>Get the latest tech news</h1>
+    <section class="featured-posts">
+      <ul>
+        <li><nuxt-link :to="'/posts/' + 1">Post title</nuxt-link></li>
+      </ul>
+    </section>
   </div>
 </template>
-
