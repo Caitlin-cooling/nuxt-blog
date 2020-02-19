4c3ed936d4537731b6e25eecc388b090c979d168
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-02-19 11:53:23 +0000

    Minor changes

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container">
-    <h1>This is an the page for post {{ $route.params.id }}</h1>
+    <h1>Title of the post</h1>
+    <div class="post-details">
+      <p>Last updated on</p>
+      <p>Written by: </p>
+    </div>
+    <p>Content of the post</p>
   </div>
 </template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <h1>This is the posts page</h1>
     <h3>Please select a post that you would like to see</h3>
-    <nuxt-link to="/posts/1">Post 1</nuxt-link>
-    <nuxt-link to="/posts/2">Post 2</nuxt-link>
-    <nuxt-link to="/posts/3">Post 3</nuxt-link>
+    <ul>
+      <li><nuxt-link :to="'/posts/' + 1">Post title</nuxt-link></li>
+    </ul>
     <nuxt-link to="/posts/new" class="new-post">Add a new post</nuxt-link>
   </div>
 </template>
