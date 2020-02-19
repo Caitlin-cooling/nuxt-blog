1f32630261edea222249c76392f86cfa4af3a9ef
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-02-19 10:52:39 +0000

    Sets up pages

---
+++ pages/about/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is the about page</h1>
+    <div class="about-content">
+      <p>Lorem ipsum dolor amet keffiyeh craft beer franzen chillwave, irony tbh four loko stumptown quinoa farm-to-table distillery tofu. Fam post-ironic vegan chillwave brunch PBR&B microdosing green juice bicycle rights four dollar toast chambray gastropub butcher tumeric. Williamsburg keffiyeh lo-fi cornhole hashtag, mustache wayfarers. Banh mi stumptown kickstarter lomo craft beer four loko roof party mixtape raclette. Fixie brunch hot chicken vexillologist. Forage brunch synth taxidermy iPhone squid kickstarter, palo santo subway tile aesthetic paleo freegan kale chips mustache before they sold out. Franzen lumbersexual hexagon ennui hoodie organic lo-fi hot chicken.</p>
+      <br/>
+      <p>Celiac fashion axe seitan neutra organic paleo artisan, selvage fam ugh art party leggings. Authentic meh kombucha air plant, next level migas vaporware hot chicken banjo sartorial organic kogi. Tbh messenger bag master cleanse helvetica waistcoat. Salvia chicharrones church-key franzen, 90's vegan trust fund austin pinterest kogi beard stumptown. Air plant single-origin coffee tote bag tacos, bushwick salvia stumptown. 8-bit knausgaard keytar biodiesel, fixie occupy sriracha fam schlitz selfies. Twee crucifix hell of pitchfork health goth.</p>
+      <br/>
+      <p>Godard affogato la croix +1 you probably haven't heard of them iceland retro tousled 3 wolf moon succulents franzen. Readymade tattooed meggings vaporware. Tumblr tbh migas disrupt copper mug, chicharrones occupy roof party cray. Gentrify woke retro hammock, taxidermy kinfolk put a bird on it seitan copper mug. Slow-carb ethical ramps af activated charcoal humblebrag chicharrones hammock.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .about {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .about h1 {
+    padding: 20px;
+  }
+
+  .about-content {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
